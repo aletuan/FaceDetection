a6d777455ea7acef9f584c14443b2d4f630e89c5
Le Tuan Anh <anhlt1983@gmail.com>
2016-06-20 14:18:28 +0700

    go

--- app/src/main/java/com/packtpub/masteringopencvandroid/facedetection/MainActivity.java
+++ app/src/main/java/com/packtpub/masteringopencvandroid/facedetection/MainActivity.java
@@ -1,13 +1,168 @@
 package com.packtpub.masteringopencvandroid.facedetection;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2{
+
+    private static final String TAG = "Face Detection::MainActivity";
+
+    private final int CAMERA_PERMISSION   = 1;
+
+    private JavaCameraView      mOpenCvCameraView;
+    private boolean             mIsFrontCamera = false;
+    private boolean             isAllowed = false;
+    private MenuItem            mItemSwitchCamera = null;
+
+    private Mat mRgba;
+    private Mat mGray;
+
+    private void initializeOpenCVDependencies() {
+
+    }
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    //mOpenCvCameraView.enableView();
+                    initializeOpenCVDependencies();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON);
+
         setContentView(R.layout.activity_main);
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA);
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "Permission is not allowed. Request permission");
+
+            ActivityCompat.requestPermissions(this, new String[]
+                    {Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+
+        } else {
+            Log.d(TAG, "Permission is granted");
+            isAllowed = true;
+        }
+
+        if (isAllowed) {
+            mOpenCvCameraView = (JavaCameraView) findViewById(R.id.java_surface_view);
+            mOpenCvCameraView.setCvCameraViewListener(this);
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_10, this, mLoaderCallback);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        Log.i(TAG, "called onCreateOptionsMenu");
+        mItemSwitchCamera = menu.add("Toggle Front/Back camera");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String toastMessage;
+
+        if (item == mItemSwitchCamera) {
+            mOpenCvCameraView.setVisibility(SurfaceView.GONE);
+            mIsFrontCamera = !mIsFrontCamera;
+
+            if (mIsFrontCamera) {
+                mOpenCvCameraView = (JavaCameraView) findViewById(R.id.java_surface_view);
+                mOpenCvCameraView.setCameraIndex(1);
+                toastMessage = "Front Camera";
+            } else {
+                mOpenCvCameraView = (JavaCameraView) findViewById(R.id.java_surface_view);
+                mOpenCvCameraView.setCameraIndex(-1);
+                toastMessage = "Back Camera";
+            }
+
+            mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+            mOpenCvCameraView.setCvCameraViewListener(this);
+
+            mOpenCvCameraView.enableView();
+
+            Toast toast = Toast.makeText(this, toastMessage, Toast.LENGTH_LONG);
+
+            toast.show();
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mGray = new Mat();
+        mRgba = new Mat();
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mRgba = inputFrame.rgba();
+        Imgproc.cvtColor(mRgba, mGray, Imgproc.COLOR_BGRA2GRAY);
+        return mGray;
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
     }
 }
